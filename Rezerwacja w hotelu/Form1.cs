c5cd4732bced3035c668df9d20ed54f24d560062
Lenar12 <berta1116@g.pl>
2015-05-30 13:18:39 +0200

    Rezerwacja_Hotel v1.0

--- Rezerwacja w hotelu/Form1.cs
+++ Rezerwacja w hotelu/Form1.cs
@@ -16,5 +16,10 @@ namespace Rezerwacja_w_hotelu
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
