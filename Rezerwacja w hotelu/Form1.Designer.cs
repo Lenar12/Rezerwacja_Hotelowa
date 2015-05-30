e572c05a76419b36803ed2646f287a315a884cc7
Lenar12 <berta1116@g.pl>
2015-05-30 18:20:16 +0200

    Zmienianie Form

--- Rezerwacja w hotelu/Form1.Designer.cs
+++ Rezerwacja w hotelu/Form1.Designer.cs
@@ -28,21 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.login_label = new System.Windows.Forms.Label();
+            this.password_label = new System.Windows.Forms.Label();
+            this.login_box = new System.Windows.Forms.TextBox();
+            this.password_box = new System.Windows.Forms.TextBox();
+            this.accept_button = new System.Windows.Forms.Button();
+            this.cancel_button = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // login_label
+            // 
+            this.login_label.AutoSize = true;
+            this.login_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.login_label.Location = new System.Drawing.Point(40, 35);
+            this.login_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.login_label.Name = "login_label";
+            this.login_label.Size = new System.Drawing.Size(47, 17);
+            this.login_label.TabIndex = 0;
+            this.login_label.Text = "Login:";
+            // 
+            // password_label
+            // 
+            this.password_label.AutoSize = true;
+            this.password_label.Location = new System.Drawing.Point(40, 75);
+            this.password_label.Name = "password_label";
+            this.password_label.Size = new System.Drawing.Size(73, 17);
+            this.password_label.TabIndex = 1;
+            this.password_label.Text = "Password:";
+            // 
+            // login_box
+            // 
+            this.login_box.Location = new System.Drawing.Point(126, 35);
+            this.login_box.Name = "login_box";
+            this.login_box.Size = new System.Drawing.Size(161, 23);
+            this.login_box.TabIndex = 2;
+            // 
+            // password_box
+            // 
+            this.password_box.Location = new System.Drawing.Point(126, 75);
+            this.password_box.Name = "password_box";
+            this.password_box.PasswordChar = '*';
+            this.password_box.Size = new System.Drawing.Size(161, 23);
+            this.password_box.TabIndex = 3;
+            // 
+            // accept_button
+            // 
+            this.accept_button.Location = new System.Drawing.Point(38, 121);
+            this.accept_button.Name = "accept_button";
+            this.accept_button.Size = new System.Drawing.Size(75, 23);
+            this.accept_button.TabIndex = 4;
+            this.accept_button.Text = "Accept";
+            this.accept_button.UseVisualStyleBackColor = true;
+            this.accept_button.Click += new System.EventHandler(this.accept_button_Click);
+            // 
+            // cancel_button
+            // 
+            this.cancel_button.Location = new System.Drawing.Point(212, 121);
+            this.cancel_button.Name = "cancel_button";
+            this.cancel_button.Size = new System.Drawing.Size(75, 23);
+            this.cancel_button.TabIndex = 5;
+            this.cancel_button.Text = "Cancel";
+            this.cancel_button.UseVisualStyleBackColor = true;
+            this.cancel_button.Click += new System.EventHandler(this.cancel_button_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(311, 144);
+            this.ClientSize = new System.Drawing.Size(335, 172);
+            this.Controls.Add(this.cancel_button);
+            this.Controls.Add(this.accept_button);
+            this.Controls.Add(this.password_box);
+            this.Controls.Add(this.login_box);
+            this.Controls.Add(this.password_label);
+            this.Controls.Add(this.login_label);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Login Panel";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label login_label;
+        private System.Windows.Forms.Label password_label;
+        private System.Windows.Forms.TextBox login_box;
+        private System.Windows.Forms.TextBox password_box;
+        private System.Windows.Forms.Button accept_button;
+        private System.Windows.Forms.Button cancel_button;
+
     }
 }
 
--- Rezerwacja w hotelu/Form1.cs
+++ Rezerwacja w hotelu/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Rezerwacja_w_hotelu
 {
@@ -20,6 +21,57 @@ namespace Rezerwacja_w_hotelu
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void cancel_button_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void accept_button_Click(object sender, EventArgs e)
+        {
+            SqlConnection connect = sql_connection.GetConnection();
+            SqlDataAdapter select_com = new SqlDataAdapter("SELECT COUNT(*) FROM uzytkownicy WHERE login='"+login_box.Text+"' and haslo='"+password_box.Text+"'",connect);
+            DataTable dt = new DataTable();
+            select_com.Fill(dt);
+            
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                SqlCommand cmd = connect.CreateCommand();
+                Form form;
+
+                cmd.CommandText="SELECT uprawnienia FROM uzytkownicy WHERE login='" + login_box.Text + "'";
+                cmd.CommandType = CommandType.Text;
+               
+                connect.Open();
+                
+                SqlDataReader reader=cmd.ExecuteReader();
+
+                while(reader.Read())
+                {
+                    this.Hide();
+                    switch (reader["uprawnienia"].ToString())
+                    {
+                        case "1":           //forma recepcjonisty
+                            form = new Form2();
+                            break;
+                        case "2":           //forma administratora
+                            form = new Form3();
+                            break;
+                        case "3":           //forma sprzątaczki
+                            //form= new Form4();
+                            break;
+                    };
+                    
+                    //form.Show();
+                }
+            }
+            else
+                MessageBox.Show("Some invalid entries have been detected.","Wrong password",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            
+
+            connect.Close();
+          
         }
     }
 }
