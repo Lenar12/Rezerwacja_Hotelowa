accdc2d60f9ec76352f77ae2ecb692393716a0dc
Lenar12 <berta1116@g.pl>
2015-06-01 21:50:27 +0200

    Nowa klasa

---
+++ Rezerwacja w hotelu/klasa_testowa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rezerwacja_w_hotelu
+{
+    class klasa_testowa
+    {
+        void test()
+        {
+            //nic ciekawego
+        }
+    }
+}
