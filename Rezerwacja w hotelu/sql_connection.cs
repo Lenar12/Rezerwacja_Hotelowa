82fe76d9159b2c18061babc44ac9aef5232a1b6a
Lenar12 <berta1116@g.pl>
2015-06-01 21:16:25 +0200

    Zmiana ścieżki

--- Rezerwacja w hotelu/sql_connection.cs
+++ Rezerwacja w hotelu/sql_connection.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Windows.Forms;
 
 namespace Rezerwacja_w_hotelu
 {
@@ -12,7 +13,7 @@ namespace Rezerwacja_w_hotelu
         public static SqlConnection GetConnection()
         {
             String connStr = @"Data Source=(LocalDB)\v11.0;";
-            connStr += @"AttachDBfilename=""D:\Programy\Rezerwacja w hotelu\Rezerwacja w hotelu\baza_danych.mdf"";";
+            connStr += @"AttachDBfilename=""|DataDirectory|\baza_danych.mdf"";";
             connStr += @"Integrated Security=True";
             SqlConnection connection = new SqlConnection(connStr);
             return connection;
